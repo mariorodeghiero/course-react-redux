ff7d9b98e0965d3b0ad0735b403d039fe5d755c9
mariorodeghiero <mario.rodeghiero@gmail.com>
2018-05-07 23:24:56 -0300

    stylized audio preview and creat pause/play

--- musicmaster/src/Gallery.jsx
+++ musicmaster/src/Gallery.jsx
@@ -1,13 +1,42 @@
 import React, { Component } from 'react';
 
 class Gallery extends Component {
-    state = {  }
+    constructor(props){
+        super(props);
+        this.state = {
+            playingUrl: '',
+            audio: null,
+            playing: false
+        }
+    }
 
     playAudio(previewUrl){
         let audio = new Audio(previewUrl);
-        audio.play()
+        if(!this.state.playing){
+            audio.play()
+            this.setState({
+                playing: true,
+                playingUrl: previewUrl,
+                audio
+            })
+        } else {
+            if (this.state.playingUrl === previewUrl) {
+                this.state.audio.pause();
+                this.setState({
+                    playing: false
+                })
+            } else {
+                this.state.audio.pause();
+                audio.play();
+                this.setState({
+                    playing:true,
+                    playingUrl: previewUrl,
+                    audio
+                })
+            }
+        }
     }
-
+        
     render() {
         const { tracks } = this.props;
         return (
@@ -17,7 +46,12 @@ class Gallery extends Component {
                     return(
                         <div key={index} className="track" onClick={() => this.playAudio(track.preview_url)}>
                             <img src={trackImg} className="track-img" alt="track"/>
-                            <p className="track-img" alt="track"></p>
+                            <div className="track-play">
+                                <div className="track-play-inner">
+                                    {this.state.playingUrl === track.preview_url ? <span>| |</span> : <span>&#9654;</span>}
+                                </div>
+                            </div>
+                            <p className="track-text" alt="track">{track.name}</p>
                         </div>
                     )
                 })}
