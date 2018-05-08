1816b9665b8ee9d961e8fa65f22bb122dfbee820
mariorodeghiero <mario.rodeghiero@gmail.com>
2018-05-07 22:49:45 -0300

    created audio preview

--- musicmaster/src/Gallery.jsx
+++ musicmaster/src/Gallery.jsx
@@ -2,6 +2,12 @@ import React, { Component } from 'react';
 
 class Gallery extends Component {
     state = {  }
+
+    playAudio(previewUrl){
+        let audio = new Audio(previewUrl);
+        audio.play()
+    }
+
     render() {
         const { tracks } = this.props;
         return (
@@ -9,7 +15,7 @@ class Gallery extends Component {
                 {tracks.map((track, index) => {
                     const trackImg = track.album.images[0].url;
                     return(
-                        <div key={index} className="track">
+                        <div key={index} className="track" onClick={() => this.playAudio(track.preview_url)}>
                             <img src={trackImg} className="track-img" alt="track"/>
                             <p className="track-img" alt="track"></p>
                         </div>
